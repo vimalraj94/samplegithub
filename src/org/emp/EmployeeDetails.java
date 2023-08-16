c124ddf0a46d5dbfbfed3a098607f0180f041658
vimalraj s <vraj97515@gmail.com>
2023-08-16 10:24:03 -0700

    sample practice

---
+++ src/org/emp/EmployeeDetails.java
@@ -0,0 +1,18 @@
+package org.emp;
+//parent class
+public class EmployeeDetails {
+	public static void main (String[] args) {
+		byte sNo=48;
+		short atmPin=1448;
+		int accNo=1234567;
+		long mobileNo=9080012240l;
+		float percentage=98.1234567f;
+		double overallPercentage=99.12345678923;
+		char initial='S';
+		String name="Vimalraj sivaraj";
+		boolean married=false;
+		System.out.println(sNo+ "\n" +atmPin+ "\n" +accNo+ "\n" +mobileNo+ "\n" +percentage+ "\n" +overallPercentage+ "\n" +initial+ "\n" +name+ "\n" +married);
+				
+	}
+
+}
